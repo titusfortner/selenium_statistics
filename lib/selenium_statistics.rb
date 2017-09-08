9972a5e31e34044e87ca94dd2a871f4f1cfa0896
Titus Fortner <titusfortner@gmail.com>
2017-09-08 15:16:22 -0700

    implement logger

--- lib/selenium_statistics.rb
+++ lib/selenium_statistics.rb
@@ -1,4 +1,17 @@
 require 'selenium-webdriver'
+require 'selenium_statistics/logger'
+
+#
+# Returns logger instance that can be used throughout Selenium Run.
+#
+# @return [Logger]
+#
+
+module SeleniumStatistics
+  def self.logger
+    @logger ||= SeleniumStatistics::Logger.new
+  end
+end
 
 require "selenium_statistics/version"
 require 'selenium_statistics/statistics'
